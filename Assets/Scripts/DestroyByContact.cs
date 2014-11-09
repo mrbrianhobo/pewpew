b20cfafd4e05f31c164058a9f084aca823a4f5f8
mrbrianhobo <mr.brianhobo@hotmail.com>
2014-11-09 07:30:08 -0800

    ughh unity qq

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class DestroyByContact : MonoBehaviour {
 
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	
 	void OnTriggerEnter(Collider other){
 		if (other.tag == "Boundary"){
 			return;
+		}
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player"){
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			//gameController.GameOver ();
 		}
 		Destroy(other.gameObject);
 		Destroy(gameObject);
